f550d1080db0774ac77d449bae75bd3725f12acd
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-09 04:02:41 +0100

    update

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ console.log("Console OS initialized successfully")
 
 console.log("Console OS")
 setTimeout((1), console.log("by 0x10variacie"));
-console.log("Waiting...")
+console.log("Waiting...");
 let prompt = document.getElementById("TypePrompt");
-
+prompt = String(prompt);
