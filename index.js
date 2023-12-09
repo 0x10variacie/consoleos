201cc6d423b6e3f9d151778add61cfc0ae631769
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-09 03:45:12 +0100

    core update

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 console.log("Console OS initialized successfully")
 
 console.log("Console OS")
-setTimeout((1), {
-    console.log("by 0x10variacie");
-}, timeout);
+setTimeout((1), console.log("by 0x10variacie"));
 let prompt = document.getElementById("TypePrompt").value;
