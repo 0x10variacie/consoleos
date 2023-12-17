b305979218d997fadf034713df3ec56109e62b25
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-17 04:41:29 +0100

    upd

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ console.log("Waiting...");
 let prompt = document.getElementById("TypePrompt");
 prompt = String(prompt);
 
-document.getElementById("")
+document.getElementById("BSubmit").onclick = function() {
+    console.log(prompt);
+    if (prompt="about") {
+        console.log("Please expand");
+        console.log("ConsoleOS v.DEV");
+        console.log("made by 0x10variacie");
+    }
+    else if (prompt="help") {
+        console.log("Please expand");
+        console.log("help - displays this page")
+        console.log("about - displays information about this console")
+    }
+    
+}
