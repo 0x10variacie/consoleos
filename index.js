ec3abb7ca9fa626c2718ab402f2cd9007c9559cb
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-17 04:59:47 +0100

    upd

--- index.js
+++ index.js
@@ -4,25 +4,26 @@ console.log("Console OS")
 setTimeout((1), console.log("by 0x10variacie"));
 console.log("Waiting...");
 
-document.getElementById("BSubmit").onclick = switch (document.getElementById("TypePrompt".value)) {
-let prompt = document.getElementById("TypePrompt");
-prompt = String(prompt);
-    case prompt = "about":
-        console.log("Please expand");
-        console.log("ConsoleOS v.DEV");
-        console.log("made by 0x10variacie");        
-        break;
-    case prompt = "help":
-        console.log("Please expand");
-        console.log("help - displays this page");
-        console.log("about - displays information about this console");      
-        break;
-    case prompt = "calc":
-        console.log("Please expand");
-        console.log("Send me the 1st value");
-        console.log("Under development");
-        break;
+document.getElementById("BSubmit").onclick = function() {
+    switch (document.getElementById("TypePrompt".value)) {
+        let prompt = document.getElementById("TypePrompt");
+        prompt = String(prompt);
+        case prompt = "about":
+            console.log("Please expand");
+            console.log("ConsoleOS v.DEV");
+            console.log("made by 0x10variacie");        
+            break;
+        case prompt = "help":
+            console.log("Please expand");
+            console.log("help - displays this page");
+            console.log("about - displays information about this console");      
+            break;
+        case prompt = "calc":
+            console.log("Please expand");
+            console.log("Send me the 1st value");
+            console.log("Under development");
+            break;
 
-    default:
-        break;
+        default:
+            break;
 }
