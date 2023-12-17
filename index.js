deccecfe9d816530fc995b7c2bbf492e2201a3f6
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-17 04:37:44 +0100

    upd

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ setTimeout((1), console.log("by 0x10variacie"));
 console.log("Waiting...");
 let prompt = document.getElementById("TypePrompt");
 prompt = String(prompt);
+
+document.getElementById("")
